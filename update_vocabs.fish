1d5441e7f437af24f8a23e588512206c5393ac80
phette23 <phette23@gmail.com>
2023-06-13 13:55:34 -0700

    update comments in update_vocabs
    which I don't think will even work with the local services repo

--- update_vocabs.fish
+++ update_vocabs.fish
@@ -1,8 +1,9 @@
 #!/usr/bin/env fish
+# waiting to have this add_to_fixture cmd so we don't have to rebuild fixtures every time they change
+# https://github.com/inveniosoftware/invenio-rdm-records/pull/1303
 docker cp app_data invenio-web-ui-1:/opt/invenio/src
 echo "entering docker container so you can run the command"
 echo "invenio rdm-records fixtures && invenio rdm-records rebuild-index (it's on your clipboard)"
 echo "invenio rdm-records fixtures && invenio rdm-records rebuild-index" | pbcopy
-echo "but Eric why don't you just 'docker exec' the command in this script?"
-echo "great question, it's because docker is a piece of shit & that inexplicably doesn't work"
+# `docker run` the command above does not work for some reason, env vars?
 docker exec -it invenio-web-ui-1 /bin/bash
