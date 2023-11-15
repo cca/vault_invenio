7a0cfafcd6c4588f62b3d69fa25098193da465ab
phette23 <phette23@gmail.com>
2023-11-15 14:54:06 -0800

    fix post.py script that creates record w/ attachment via API

--- notes/code_samples/post.py
+++ notes/code_samples/post.py
@@ -1,10 +1,23 @@
+"""
+POST record metadata and file to InvenioRDM REST API
+
+See Invenio's example:
+https://github.com/inveniosoftware/docs-invenio-rdm-restapi-example"""
 import json
+import os
+import sys
 import requests
 
 # shut up urllib3 SSL verification warning
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning
+)
 
-token = 'L7yvVDVBMJ8wx0WPVgXbQ2C83xTQNxO2yyvSHeKr9lU1fdIg0S5nX0Fy1sOd'
+token = os.environ.get("TOKEN", False) or os.environ.get("INVENIO_TOKEN", False)
+if not token:
+    raise Exception(
+        "Error: provide a personal access token in the TOKEN or INVENIO_TOKEN env var"
+    )
 # at least 5 HTTP requests to creating an item with a single attachment:
 # 1) create metadata record as draft
 # 2) for each attachment
@@ -13,42 +26,68 @@ token = 'L7yvVDVBMJ8wx0WPVgXbQ2C83xTQNxO2yyvSHeKr9lU1fdIg0S5nX0Fy1sOd'
 #   c) commit the file to the draft
 # 3) publish the draft
 
+
 def result(response):
-    print('HTTP {}'.format(response.status_code))
+    print("HTTP {}".format(response.status_code))
     print(response.text)
+    response.raise_for_status()
 
 
-with open('record.json', 'r') as fh:
+with open("./notes/code_samples/record.json", "r") as fh:
     data = json.load(fh)
     headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {}'.format(token)
+        "Content-Type": "application/json",
+        "Authorization": "Bearer {}".format(token),
     }
     # create metadata-only draft
-    response = requests.post('https://127.0.0.1/api/records', json=data, verify=False, headers=headers)
+    response = requests.post(
+        "https://127.0.0.1:5000/api/records", json=data, verify=False, headers=headers
+    )
 
 result(response)
 draft = response.json()
-id = draft['id']
+id = draft["id"]
 
 # add file names to draft
 file_keys = [{"key": "syllabus.pdf"}]
-response = requests.post('https://127.0.0.1/api/records/{}/draft/files'.format(id), json=file_keys, headers=headers, verify=False)
+response = requests.post(
+    "https://127.0.0.1:5000/api/records/{}/draft/files".format(id),
+    json=file_keys,
+    headers=headers,
+    verify=False,
+)
 result(response)
 
 # PUT file data to draft
-with open ('syllabus.pdf', 'rb') as file:
-    headers['Content-Type'] = 'application/octet-stream'
-    response = requests.put('https://127.0.0.1/api/records/{}/draft/files/{}/content'.format(id, 'syllabus.pdf'), data=file, headers=headers, verify=False)
+with open("./notes/code_samples/syllabus.pdf", "rb") as file:
+    headers["Content-Type"] = "application/octet-stream"
+    response = requests.put(
+        "https://127.0.0.1:5000/api/records/{}/draft/files/{}/content".format(
+            id, "syllabus.pdf"
+        ),
+        data=file,
+        headers=headers,
+        verify=False,
+    )
     result(response)
 
 # commit file
-headers['Content-Type']= 'application/json'
-response = requests.post('https://127.0.0.1/api/records/{}/draft/files/{}/commit'.format(id, 'syllabus.pdf'), headers=headers, verify=False)
+headers["Content-Type"] = "application/json"
+response = requests.post(
+    "https://127.0.0.1:5000/api/records/{}/draft/files/{}/commit".format(
+        id, "syllabus.pdf"
+    ),
+    headers=headers,
+    verify=False,
+)
 result(response)
 
 # publish draft
-response = requests.post('https://127.0.0.1/api/records/{}/draft/actions/publish'.format(id), headers=headers, verify=False)
+response = requests.post(
+    "https://127.0.0.1:5000/api/records/{}/draft/actions/publish".format(id),
+    headers=headers,
+    verify=False,
+)
 result(response)
 # link to (HTML version of) published record
-print(json.loads(response.text)['links']['self_html'])
+print(json.loads(response.text)["links"]["self_html"])
